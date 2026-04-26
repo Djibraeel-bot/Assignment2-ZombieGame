992f8e9f7ad72bc62ef25963451e7ac0222567d6
therealthori <2570507@students.wits.ac.za>
2026-04-27 00:27:40 +0200

    Reapply "First Things"
    This reverts commit 3a82170d585a1dc5a7dac67d65d7a0b9c5544c66.

---
+++ ZombieGame/Assets/ThoriScript/Pickup.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int throwableIndex;
+    public int amount = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Pickup touched: " + other.name);
+
+        ObjectLogic inventory = other.GetComponentInParent<ObjectLogic>();
+
+        if (inventory == null)
+        {
+            Debug.Log("NO ObjectLogic found on " + other.name);
+            return;
+        }
+
+        Debug.Log("FOUND inventory on: " + inventory.name);
+        Debug.Log("Trying to add index: " + throwableIndex + " amount: " + amount);
+
+        inventory.AddThrowable(throwableIndex, amount);
+
+        Destroy(gameObject);
+    }
+}
